0198ab89e7d3de2502bf26dda3664857a8981209
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-05-28 15:21:10 +0200

    Using a runtime controller

---
+++ src/app/RemoteDuck/Model/Runtime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RemoteDuck.Model
+{
+    public class Runtime
+    {
+        readonly DateTime _startTime;
+
+        public Runtime()
+        {
+            _startTime = DateTime.Now;
+        }
+
+        public double ElapsedTime
+        {
+            get { return (DateTime.Now - _startTime).TotalSeconds; }
+        }
+
+        public string GetElapsedTimeText()
+        {
+            return string.Format("Runtime: {0}s", Math.Round(ElapsedTime));
+        }
+    }
+}
--- src/app/RemoteDuck/RemoteDuck.cs
+++ src/app/RemoteDuck/RemoteDuck.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Windows.Forms;
+using RemoteDuck.Model;
 
 namespace RemoteDuck
 {
     public partial class RemoteDuck : Form
     {
-        readonly DateTime _startTime;
+        readonly Runtime _runtime;
 
         public RemoteDuck()
         {
             InitializeComponent();
             toolStripStatusLabel1.Text = "";
-            _startTime = DateTime.Now;
+            _runtime = new Runtime();
             timer1.Enabled = true;
         }
 
         void TimerTicked(object sender, EventArgs e)
         {
-            toolStripStatusLabel1.Text = string.Format("Runtime: {0}s", Math.Round((DateTime.Now - _startTime).TotalSeconds));
+            toolStripStatusLabel1.Text = _runtime.GetElapsedTimeText();
         }
     }
 }
