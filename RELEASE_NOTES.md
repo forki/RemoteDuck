790a86f0c9349b5eefa450372b24b11804df1c86
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-20 11:51:04 +0100

    Create a FAKE build

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 0.4.0 - 31.12.2013
+* Initial FAKE build
---
+++ build.fsx
@@ -0,0 +1,92 @@
+// --------------------------------------------------------------------------------------
+// FAKE build script 
+// --------------------------------------------------------------------------------------
+
+#r @"packages/FAKE/tools/FakeLib.dll"
+open Fake 
+open Fake.Git
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
+open System
+
+let projects = [|"RemoteDuck"|]
+
+let summary = "Various type providers for management of the machine."
+let description = "RemoteDuck is a tool which allows you to show that a remote session is blocked."
+let authors = ["Steffen Forkmann"]
+let tags = "F# remote rdp"
+
+let solutionFile  = "RemoteDuck"
+
+let testAssemblies = "tests/**/bin/Release/*.Tests*.dll"
+let nugetDir = "./nuget/"
+
+// Read additional information from the release notes document
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let release = parseReleaseNotes (IO.File.ReadAllLines "RELEASE_NOTES.md")
+
+// Generate assembly info files with the right version & up-to-date information
+Target "AssemblyInfo" (fun _ ->
+  for project in projects do
+    let fileName = "src/" + project + "/Properties/AssemblyInfo.cs"
+    CreateCSharpAssemblyInfo fileName
+        [ Attribute.Title project
+          Attribute.Product project
+          Attribute.Description summary
+          Attribute.Version release.AssemblyVersion
+          Attribute.FileVersion release.AssemblyVersion ] 
+)
+
+// --------------------------------------------------------------------------------------
+// Clean build results & restore NuGet packages
+
+Target "RestorePackages" RestorePackages
+
+Target "Clean" (fun _ ->
+    CleanDirs ["bin"; "temp"; nugetDir]
+)
+
+// --------------------------------------------------------------------------------------
+// Build library & test project
+
+Target "Build" (fun _ ->
+    !! (solutionFile + ".sln")
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
+)
+
+// --------------------------------------------------------------------------------------
+// Run the unit tests using test runner & kill test runner when complete
+
+Target "RunTests" (fun _ ->
+    ActivateFinalTarget "CloseTestRunner"
+
+    !! testAssemblies
+    |> NUnit (fun p ->
+        { p with
+            DisableShadowCopy = true
+            TimeOut = TimeSpan.FromMinutes 20.
+            OutputFile = "TestResults.xml" })
+)
+
+FinalTarget "CloseTestRunner" (fun _ ->  
+    ProcessHelper.killProcess "nunit-agent.exe"
+)
+
+Target "Release" DoNothing
+
+// --------------------------------------------------------------------------------------
+// Run all targets by default. Invoke 'build <Target>' to override
+
+Target "All" DoNothing
+
+"Clean"
+  ==> "RestorePackages"
+  ==> "AssemblyInfo"
+  ==> "Build"
+  ==> "All"
+
+"All" 
+  ==> "Release"
+
+RunTargetOrDefault "All"
